bb3f4020422a995ed1e6ff260933b99e584ff035
Age Mooij <46568+agemooij@users.noreply.github.com>
2020-05-29 17:05:57 +0200

    Github action to deploy a Kaizo microservice to a GKE cluster (#1)
    Please notice that this is a public repository! that is because Github Actions currently doesn't support loading actions from private repositories, or at least not without a some tricky workarounds. I do not think there is anything proprietary in this repo and the Github action is quite specific for Kaizo. That said, it could serve as a public OSS example of how to do this kind of stuff
    
    This action uses the following tools:
    - The Google Cloud SDK
    - Kustomize
    - kubectl
    - Bash, with envsubst for some very basic templating to set the project name in the base resource files
    
    Interesting files:
    - The entrypoint.sh file does most of the work
    - The action.yaml file contains all the metadata for how this action should be used
    - The Dockerfile is a custom version of the Google cloud-sdk for Alpine with kustomize and kubectl added.
    
    Example usage:
    - The PR in dojo-master that uses this action
    - A successful build of that PR (check its full output log for all the details)

---
+++ deploy-microservice/entrypoint.sh
@@ -0,0 +1,123 @@
+#! /usr/bin/env bash
+
+if [ $# -ne 8 ]; then
+  echo "Not enough arguments supplied"
+  echo "Usage: entrypoint.sh [SERVICE_NAME] [TARGET_ENV] [REVISION] [IMAGE] [GCP_PROJECT_ID] [GCP_CLUSTER_NAME] [GCP_ZONE] [GCP_SERVICE_ACCOUNT_KEY]"
+  exit 1
+fi
+
+export SERVICE_NAME=$1
+export TARGET_ENV=$2
+export REVISION=$3
+export IMAGE=$4
+export GCP_PROJECT_ID=$5
+export GCP_CLUSTER_NAME=$6
+export GCP_ZONE=$7
+export GCP_SERVICE_ACCOUNT_KEY=$8
+
+echo "SERVICE_NAME:     $SERVICE_NAME"
+echo "TARGET_ENV:       $TARGET_ENV"
+echo "REVISION:         $REVISION"
+echo "IMAGE:            $IMAGE"
+echo "GCP_PROJECT_ID:   $GCP_PROJECT_ID"
+echo "GCP_CLUSTER_NAME: $GCP_CLUSTER_NAME"
+echo "GCP_ZONE:         $GCP_ZONE"
+echo ""
+echo "HOME:             $HOME"
+echo "PWD:              $PWD"
+echo "Working Dir:      $GITHUB_WORKSPACE"
+echo "Repo:             $GITHUB_REPOSITORY"
+echo "Sha:              $GITHUB_SHA"
+echo ""
+echo "Kustomize:"
+echo "    $(kustomize version)"
+echo "Kubectl:"
+echo "    $(kubectl version --client)"
+
+KUSTOMIZE_DIR=".github/k8s/"
+TEMPLATES_DIR="/templates/"
+
+echo ""
+echo "Contents of project patches dir:"
+ls -al $KUSTOMIZE_DIR
+
+echo ""
+echo "Setting gcloud defaults..."
+gcloud config set core/project $GCP_PROJECT_ID
+gcloud config set compute/zone $GCP_ZONE
+
+echo ""
+echo "Writing the GCP service account credentials to a tmp file..."
+SERVICE_ACCOUNT_KEY_FILE=$(mktemp)
+echo "$GCP_SERVICE_ACCOUNT_KEY" | base64 -d > $SERVICE_ACCOUNT_KEY_FILE
+
+echo "Authenticating gcloud for project '$GCP_PROJECT_ID'..."
+gcloud auth activate-service-account --key-file=$SERVICE_ACCOUNT_KEY_FILE
+
+echo "Authenticating kubectl for cluster '$GCP_CLUSTER_NAME'..."
+gcloud container clusters get-credentials $GCP_CLUSTER_NAME
+
+echo ""
+echo "Make sure we can connect to the cluster."
+echo "Fetching the target namespace..."
+kubectl get namespace ${TARGET_ENV} -o yaml | sed 's/^/  /'
+
+echo ""
+echo "Initializing kustomizations..."
+cd $KUSTOMIZE_DIR
+touch kustomization.yaml
+kustomize edit set namespace $TARGET_ENV
+
+echo "Generating kustomization base resources..."
+mkdir base
+envsubst < $TEMPLATES_DIR/deployment-base.yaml > base/deployment-base.yaml
+envsubst < $TEMPLATES_DIR/service-base.yaml > base/service-base.yaml
+kustomize edit add resource base/deployment-base.yaml
+kustomize edit add resource base/service-base.yaml
+
+echo "Adding build info kustomizations..."
+kustomize edit add annotation gitRev:$REVISION
+kustomize edit set image $SERVICE_NAME=$IMAGE
+
+echo "Adding project-specific patches..."
+for patch in *.yaml; do
+  if [ "$patch" != "kustomization.yaml" ]; then
+    echo "  Adding patch '${patch}'..."
+    kustomize edit add patch ${patch}
+  fi
+done
+
+if [ -d $TARGET_ENV ]; then
+  for patch in $TARGET_ENV/*.yaml; do
+      echo "  Adding patch '${patch}'..."
+      kustomize edit add patch ${patch}
+  done
+fi
+
+echo "Contents of kustomization.yaml:"
+cat kustomization.yaml | sed 's/^/  /'
+
+echo ""
+echo "Kustomizing resources..."
+kustomize build . > kustomized.yaml
+cat kustomized.yaml | sed 's/^/  /'
+
+echo ""
+echo "Deploying to ${TARGET_ENV}..."
+kubectl --namespace $TARGET_ENV apply -f kustomized.yaml --record
+kubectl --namespace $TARGET_ENV rollout status deployment.v1.apps/${SERVICE_NAME}-deployment
+
+echo ""
+echo "Updated Resources:"
+echo "=========================================================="
+kubectl --namespace $TARGET_ENV describe -f kustomized.yaml
+
+echo "=========================================================="
+echo "Updated Pods:"
+echo "=========================================================="
+kubectl --namespace $TARGET_ENV get pod | grep $SERVICE_NAME
+
+echo "=========================================================="
+echo "Updated Service:"
+echo "=========================================================="
+kubectl --namespace $TARGET_ENV get svc | grep $SERVICE_NAME
